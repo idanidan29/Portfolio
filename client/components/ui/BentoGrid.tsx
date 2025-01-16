decd088ba6891b9b7682e344997c552c6aa078a4
Idan Levi <29idan29@gmail.com>
2025-01-16 18:01:53 +0200

    update data

--- client/components/ui/BentoGrid.tsx
+++ client/components/ui/BentoGrid.tsx
@@ -66,7 +66,7 @@ export const BentoGridItem = ({
   };
 */
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "29idan29@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
