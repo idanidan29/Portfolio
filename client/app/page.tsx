9a43d3a1fcbe60d546d556e41aee325841d88637
Idan Levi <29idan29@gmail.com>
2025-01-16 05:30:00 +0200

    adding globe from three

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import Hero from "@/components/Hero";
+import { navItems } from "@/data";
 
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-      
-       <Hero/>
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
  
       </div>
     </main>
