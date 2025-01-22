437760168fb1f349157ede459d5b4a8a5fe9a3e5
Idan Levi <29idan29@gmail.com>
2025-01-22 15:10:44 +0200

    adding copyright

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -34,6 +34,9 @@ const Footer = () => {
       </div>
 
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+      <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2025 Idan Levi
+        </p>
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
             <a
