31f2c06d1804a8d3e6a68d9919ce951483c315c8
Idan Levi <29idan29@gmail.com>
2025-01-23 21:45:54 +0200

    trying (again) to make the globe animation work

---
+++ sentry.server.config.ts
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+
+  tracesSampleRate: 1,
+
+  debug: false,
+
+});
