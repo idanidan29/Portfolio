945fe411a2495e703f4ff2bb03dfd7aaa5ad1c7b
Idan Levi <29idan29@gmail.com>
2025-01-24 16:17:24 +0200

    Revert "samll adjusments"
    This reverts commit 1f02ad2c2b809c07ad07df73e32000b5752b3d06.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Idan's Portfolio"
+  title: "Idan's Portfolio",
+  description: "Modern & Minimal JS Mastery Portfolio",
 };
 
 export default function RootLayout({
