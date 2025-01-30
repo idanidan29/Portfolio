31010995984515df35d0b4084b4d15be7c1389e2
Idan Levi <29idan29@gmail.com>
2025-01-30 14:28:28 +0200

    adding fitting pic for the grid

--- data/index.ts
+++ data/index.ts
@@ -15,8 +15,8 @@ export const navItems = [
       description: "",
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
-      titleClassName: "justify-end",
-      img: "",
+      titleClassName: "flex justify-center items-center md:max-w-full max-w-[120px] text-center mx-auto h-screen",
+      img: "galaxy.png",
       spareImg: "",
     },
     {
