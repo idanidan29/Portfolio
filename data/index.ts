aa1f29abdd64af68acc540f570eb53eef5e887d0
Idan Levi <29idan29@gmail.com>
2025-01-30 14:37:10 +0200

    size adjusments

--- data/index.ts
+++ data/index.ts
@@ -15,7 +15,7 @@ export const navItems = [
       description: "",
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
-      titleClassName: "flex justify-center items-center md:max-w-full max-w-[120px] text-center mx-auto h-screen",
+      titleClassName: "flex justify-center items-center text-center",
       img: "galaxy.png",
       spareImg: "",
     },
