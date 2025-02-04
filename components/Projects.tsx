4fb81243750145e519deab5938e0243826046951
Idan Levi <29idan29@gmail.com>
2025-02-04 14:09:34 +0200

    small bug fix

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -13,7 +13,7 @@ const Projects = () => {
                 <span className="text-purple">Recent Projects</span>
             </h1>
             {projects.map((item) => (
-                <CardContainer className="inter-var">
+                <CardContainer className="inter-var" key={item.id}>
                     <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black-200 dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                         <CardItem
                             translateZ="50"
