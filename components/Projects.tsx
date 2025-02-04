104abca844dbf8a3dc36078b9ab1ed737349d69a
Idan Levi <29idan29@gmail.com>
2025-02-04 13:54:55 +0200

    Revert "deleting the old one and updating the new"
    This reverts commit 87a44af9826d5fd9c17aa32b47378a27a26ee965.

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,10 +7,6 @@ import { AnimatedTooltip } from "./ui/animated-tooltip";
 const Projects = () => {
     return (
         <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10" id="projects">
-            <h1 className="heading">
-                A small selection of{" "}
-                <span className="text-purple">Recent Projects</span>
-            </h1>
             {projects.map((item) => (
                 <CardContainer className="inter-var">
                     <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black-200 dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
