1ff6995381491943f025c4f2065c20084e37f0f6
Idan Levi <29idan29@gmail.com>
2025-02-05 14:54:59 +0200

    adding backround for projects

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,13 @@ import Testimonies from "@/components/Testimonies";
 import Experience from "@/components/Experience";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Projects from "../components/Projects";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <BackgroundBeams/>
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
