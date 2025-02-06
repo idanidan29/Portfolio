2dfc474ff7e09fba6eda30c93a49a16fe4ee3d04
Idan Levi <29idan29@gmail.com>
2025-02-06 15:05:58 +0200

    changing the favicon to png

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/hearth.png" sizes="any" />
+        <link rel="icon" href="/blueStar4.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
